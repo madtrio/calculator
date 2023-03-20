fcfabd7bc7a1702a519a72fd001c24f99405b4ef
madtro <anna.kozitzyna@yandex.ru>
2023-03-20 22:46:50 +0300

    1

--- src/main/java/pro/sky/calculator/CalcController.java
+++ src/main/java/pro/sky/calculator/CalcController.java
@@ -16,4 +16,40 @@ public class CalcController {
     public String answerCalc() {
         return calcService.answerCalc();
     }
+
+
+    @GetMapping(path = "/calculator/plus")
+    public String plus(@RequestParam(value = "num1", required = false) Integer a, @RequestParam(value = "num2", required = false) Integer b) {
+        return buildView(a, b, calcService.plus(a, b), "+");
+    }
+
+    @GetMapping(path = "/calculator/divide")
+    public String divide(@RequestParam(value = "num1", required = false) Integer a, @RequestParam(value = "num2", required = false) Integer b) {
+
+        return buildView(a, b, calcService.divide(a, b), "/");
+    }
+
+    @GetMapping(path = "/calculator/multiply")
+    public String multiply(@RequestParam(value = "num1", required = false) Integer a, @RequestParam(value = "num2", required = false) Integer b) {
+
+        return buildView(a, b, calcService.multiply(a, b), "*");
+    }
+
+    @GetMapping(path = "/calculator/minus")
+    public String minus(@RequestParam(value = "num1", required = false) Integer a, @RequestParam(value = "num2", required = false) Integer b) {
+
+        return buildView(a, b, calcService.minus(a, b), "-");
+    }
+
+    private String buildView(Integer a, Integer b, Number result, String operation) {
+        if (a == null || b == null) {
+            return "Не передан один из параметров";
+        }
+        if ("/".equals(operation) && b == 0) {
+            return "На 0 делить нельзя";
+        }
+        return a + " " + operation + " " + b + " = " + result;
+
+    }
+
 }
--- src/main/java/pro/sky/calculator/CalcService.java
+++ src/main/java/pro/sky/calculator/CalcService.java
@@ -1,10 +1,28 @@
 package pro.sky.calculator;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Service
 public class CalcService {
     public String answerCalc() {
         return "Добро пожаловать в калькулятор";
     }
+
+    public int plus(int a, int b) {
+        return a + b;
+    }
+
+    public int minus(int a, int b) {
+        return a - b;
+    }
+
+    public double divide(int a, int b) {
+        return a / (double) b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
 }
+
